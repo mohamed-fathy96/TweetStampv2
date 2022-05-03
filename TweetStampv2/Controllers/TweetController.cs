cb9ea6ea0eb76ee18c5ccea110f096417e6feddf
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-05-03 14:33:45 +0200

    implemented @mention webhook and reply functionality

--- TweetStampv2/Controllers/TweetController.cs
+++ TweetStampv2/Controllers/TweetController.cs
@@ -11,6 +11,10 @@ using System.Diagnostics;
 using RestSharp;
 using TweetStampv2.DTOs;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Tweetinvi.Parameters;
 
 namespace TweetStamp.Controllers
 {
@@ -19,31 +23,39 @@ namespace TweetStamp.Controllers
         private readonly ITwitterClient twitterClient;
         private readonly IAccountActivityRequestHandler handler;
 
-        public TweetController(ITwitterClient client , IAccountActivityRequestHandler handler)
+        public TweetController(ITwitterClient client, IAccountActivityRequestHandler handler)
         {
             this.twitterClient = client;
-            this.handler = handler;          
+            this.handler = handler;
         }
         public void Index()
         {
-           //await client.AccountActivity.CreateAccountActivityWebhookAsync
-           //     ("development", "https://3d5b-197-38-251-132.eu.ngrok.io/Tweet/Webhook");
-           //await client.AccountActivity.SubscribeToAccountActivityAsync("development");
-           //var environmentState = await client.AccountActivity.GetAccountActivitySubscriptionsAsync("development");
-           //var userId = long.Parse(environmentState.Subscriptions[0].UserId);
-            // This requires applications credentials with a bearer token
-            //var environments = await client.AccountActivity.GetAccountActivityWebhookEnvironmentsAsync();
+            // var webhooks = await twitterClient.AccountActivity.GetAccountActivityEnvironmentWebhooksAsync("development");
+            // if (webhooks.Length > 0)
+            // {
+            //     await twitterClient.AccountActivity.DeleteAccountActivityWebhookAsync("development", webhooks[0]);
+            // }
 
-            // the sandbox environment is available within the free tier
-            //var developmentEnvironment = environments.Single(x => x.Name == "development");
+            // await twitterClient.AccountActivity.CreateAccountActivityWebhookAsync
+            //     ("development", "https://383e-197-38-226-78.eu.ngrok.io/Tweet/Webhook");
+            //await twitterClient.AccountActivity.SubscribeToAccountActivityAsync("development");
+            //var environmentState = await twitterClient.AccountActivity.GetAccountActivitySubscriptionsAsync("development");
+            //var userId = long.Parse(environmentState.Subscriptions[0].UserId);
 
-            // registered webhooks url are available in the Webhooks collection
-            //var registeredWebhooks = developmentEnvironment.Webhooks;
+            // // This requires applications credentials with a bearer token
+            // var environments = await twitterClient.AccountActivity.GetAccountActivityWebhookEnvironmentsAsync();
+
+            // // the sandbox environment is available within the free tier
+            // var developmentEnvironment = environments.Single(x => x.Name == "development");
+
+            // // registered webhooks url are available in the Webhooks collection
+            // var registeredWebhooks = developmentEnvironment.Webhooks;
 
             var accountActivityStream = handler.GetAccountActivityStream(1519700194912358400, "development");
 
             //Trace.WriteLine(environmentState.Subscriptions[0].UserId);
             accountActivityStream.MessageReceived += Webhook;
+            accountActivityStream.TweetCreated += ReplyWebhook;
         }
 
         [HttpPost]
@@ -56,7 +68,7 @@ namespace TweetStamp.Controllers
             var tweetId = long.Parse(tweetUrl.Split("status/")[1].Split('?')[0]);
             var tweet = await twitterClient.Tweets.GetTweetAsync(tweetId);
 
-            var tweetJson = JsonConvert.SerializeObject(tweet.TweetDTO, 
+            var tweetJson = JsonConvert.SerializeObject(tweet.TweetDTO,
                 Formatting.None, new JsonSerializerSettings()
                 {
                     NullValueHandling = NullValueHandling.Ignore
@@ -80,19 +92,68 @@ namespace TweetStamp.Controllers
 
             var tweetHash = Sb.ToString();
 
-            var restClient = new RestClient("https://api.originstamp.com");
-            var restRequest = new RestRequest("v4/timestamp/create", Method.Post);
-            restRequest.AddHeader("Content-Type", "application/json");
-            restRequest.AddHeader("Accept", "application/json");
-            restRequest.AddHeader("Authorization", "182700ca-cc43-4203-a08a-f250828fc979");
+            var senderId = long.Parse((string)jsonObj["direct_message_events"][0]["message_create"]["sender_id"]);
 
-            var reqBody = new TimeStampRequestDTO(null, tweetHash, null);
-            restRequest.RequestFormat = DataFormat.Json;
-            restRequest.AddBody(reqBody);
-            var restResponse = await restClient.PostAsync(restRequest);
-            var restDto = JsonConvert.DeserializeObject<TimeStampResponseDTO>(restResponse.Content);
-            Trace.WriteLine("hello");
+            await twitterClient.Messages.PublishMessageAsync($"Here's your stringified tweet's hash: " +
+                $"{tweetHash}", senderId);
 
+            //var restClient = new RestClient("https://proofofexistence.com");
+            //var restRequest = new RestRequest("api/v1/register", Method.Post);
+            //restRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            //restRequest.AddHeader("Accept", "application/json");
+            //restRequest.AddBody(new { d = tweetHash });
+
+            //var restResponse = await restClient.PostAsync(restRequest);
+
+        }
+
+        [HttpPost]
+        public async void ReplyWebhook(object sender, TweetCreatedEvent e)
+        {
+            var messageJson = e.Json;
+            JObject jsonObj = JObject.Parse(messageJson);
+            
+            if (((string)jsonObj["tweet_create_events"][0]["text"]).ToLower().Contains("stamp"))
+            {
+                var tweetUrl = (string)jsonObj["tweet_create_events"][0]["in_reply_to_status_id_str"];
+                
+                if (long.TryParse(tweetUrl, out long tweetId))
+                {
+                    var tweet = await twitterClient.Tweets.GetTweetAsync(tweetId);
+
+                    var tweetJson = JsonConvert.SerializeObject(tweet.TweetDTO,
+                        Formatting.None, new JsonSerializerSettings()
+                        {
+                            NullValueHandling = NullValueHandling.Ignore
+                        });
+
+
+                    StringBuilder Sb = new();
+
+                    using (var hash = SHA256.Create())
+                    {
+
+                        Encoding enc = Encoding.UTF8;
+
+                        byte[] result = hash.ComputeHash(enc.GetBytes(tweetJson));
+                        var stringResult = Convert.ToHexString(result);
+                        foreach (var b in result)
+                        {
+                            Sb.Append(b.ToString("x2"));
+                        }
+                    }
+                    var tweetHash = Sb.ToString();
+                    var userName = (string)jsonObj["tweet_create_events"][0]["user"]["screen_name"];
+                    var userTweetId = (string)jsonObj["tweet_create_events"][0]["id"];
+                    var reply = await twitterClient.Tweets.PublishTweetAsync(
+                        new PublishTweetParameters($"@{userName} here's your stringified tweet's hash: " +
+                        $"{tweetHash}")
+                        {
+                            InReplyToTweetId = long.Parse(userTweetId)
+                        });
+                    
+                }
+            }
         }
 
     }
